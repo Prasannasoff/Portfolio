4fee011cd1b2de1a9f993db8cf8047aa12aabd69
Prasannasoff <prasannas.22cse@kongu.edu>
2025-01-10 21:28:14 +0530

    projectPageUpdate

--- src/Components/ProjectPage.jsx
+++ src/Components/ProjectPage.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import ProjectImage from "../assets/ProjectImages/doctor.png";
 import ProjectImage2 from "../assets/ProjectImages/image.png";
 import ProjectImage3 from "../assets/ProjectImages/waterfinal2.png";
-
-import ProjectImage4 from "../assets/ProjectImages/work2!.png";
-import ProjectImage5 from "../assets/ProjectImages/ChatApp.png";
+import ProjectImage4 from "../assets/ProjectImages/ChatApp.png";
+import ProjectImage5 from "../assets/ProjectImages/work2!.png";
+import ProjectImage6 from "../assets/ProjectImages/bookStore.png";
 import DescriptionCard from "./DescriptionCard";
 import { FaArrowRight } from 'react-icons/fa';
 
@@ -31,10 +31,18 @@ export default function ProjectPage() {
         },
         {
             image: ProjectImage3, // Replace with your actual project image
-            title: "MultiChat Application",
+            title: "Gruno Analysis",
             description: 'The Doctor Appointment System is a web-based application that streamlines booking and managing doctor appointments. It enables patients to easily find and schedule appointments with healthcare professionals. Key features include real-time availability tracking, automated reminders to minimize no-show rates, and secure user authentication. This system aims to enhance healthcare delivery and improve patient engagement.',
             githubLink: "https://github.com/yourusername/project3",
             techStack: ['MERN stack']
+        },
+
+        {
+            image: ProjectImage4, // Replace with your actual project image
+            title: "Multi Chat Application",
+            description: 'The Doctor Appointment System is a web-based application that streamlines booking and managing doctor appointments. It enables patients to easily find and schedule appointments with healthcare professionals. Key features include real-time availability tracking, automated reminders to minimize no-show rates, and secure user authentication. This system aims to enhance healthcare delivery and improve patient engagement.',
+            githubLink: "https://github.com/yourusername/project3",
+            techStack: ['React', 'Spring Boot', 'MySQL', 'Socket']
         },
 
         {
@@ -42,20 +50,12 @@ export default function ProjectPage() {
             title: "Guest House Booking",
             description: 'The Doctor Appointment System is a web-based application that streamlines booking and managing doctor appointments. It enables patients to easily find and schedule appointments with healthcare professionals. Key features include real-time availability tracking, automated reminders to minimize no-show rates, and secure user authentication. This system aims to enhance healthcare delivery and improve patient engagement.',
             githubLink: "https://github.com/yourusername/project3",
-            techStack: ['React', 'Spring Boot', 'MySQL', 'Socket']
-        },
-
-        {
-            image: ProjectImage4, // Replace with your actual project image
-            title: "Book Store",
-            description: 'The Doctor Appointment System is a web-based application that streamlines booking and managing doctor appointments. It enables patients to easily find and schedule appointments with healthcare professionals. Key features include real-time availability tracking, automated reminders to minimize no-show rates, and secure user authentication. This system aims to enhance healthcare delivery and improve patient engagement.',
-            githubLink: "https://github.com/yourusername/project3",
             techStack: ['MERN stack']
         },
 
         {
-            image: ProjectImage5, // Replace with your actual project image
-            title: "Water Monitoring",
+            image: ProjectImage6, // Replace with your actual project image
+            title: "Book Store",
             description: 'The Doctor Appointment System is a web-based application that streamlines booking and managing doctor appointments. It enables patients to easily find and schedule appointments with healthcare professionals. Key features include real-time availability tracking, automated reminders to minimize no-show rates, and secure user authentication. This system aims to enhance healthcare delivery and improve patient engagement.',
             githubLink: "https://github.com/yourusername/project3",
             techStack: ['MERN stack']
